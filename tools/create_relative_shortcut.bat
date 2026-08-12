7b9ca26fedee8c15375f1ff00d3cd081b9bd5b11
dougy147 <16362303+dougy147@users.noreply.github.com>
2026-08-12 16:06:47 +0200

    windows: auto create relative shortcut

--- tools/create_relative_shortcut.bat
+++ tools/create_relative_shortcut.bat
@@ -1,16 +1,15 @@
 :: Create "true" relative shortcut for windows
 @echo off
-set "TARGET=%CD%\src\main-windows.exe"
+set "TARGET=%COMSPEC%"
+set "ARGUMENTS=/C cd .\src && .\main-windows.exe"
 set "LINK=%CD%\main.lnk"
-::set "ICON=%CD%\src\mgico.ico"
+set "ICON=%CD%\src\icon.ico"
 
 powershell -NoProfile -Command ^
   "$shell=New-Object -ComObject WScript.Shell; " ^
   "$s=$shell.CreateShortcut('%LINK%'); " ^
-  "$s.TargetPath='powershell.exe'; " ^
-  "$s.WorkingDirectory='%CD%\src';" ^
-  "$s.Arguments='-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command & \"%TARGET%\"'; " ^
+  "$s.TargetPath='%TARGET%'; " ^
+  "$s.WorkingDirectory='';" ^
+  "$s.Arguments='%ARGUMENTS%'; " ^
+  "$s.IconLocation='%ICON%'; " ^
   "$s.Save()"
-
-:: put this before Save() if ICON path provided  
-  ::  "$s.IconLocation='%ICON%'; " ^
